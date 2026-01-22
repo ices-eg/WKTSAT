d74f329f3f4f0307927e330452a53805c94d831a
Petur Mikkelsen <peturm@hav.fo>
2026-01-22 11:04:20 +0000

    Update boot.R

--- src/FLSAM/boot.R
+++ src/FLSAM/boot.R
@@ -1,3 +1,13 @@
+# ====================================================================
+# Prepare intercatch data for SAM assessment
+# Authors: participants of workshop on the validation of new tool for refpts
+#     Petur Mikkelsen (FARMI) <peturm@hav.fo>
+#     Niels Hintzen (PFA) <nhintzen@pelagicfish.eu>
+#
+# Distributed under the terms of the MIT
+# ====================================================================
+
+
 # bootstrap.R
 
 # set the path to Rtools
